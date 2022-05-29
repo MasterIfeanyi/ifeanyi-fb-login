e705e73c32d457855ec7936b2c9c6bde1c3f5edc
Ifeanyi <chimaifeanyi29@gmail.com>
2022-05-29 02:52:08 +0100

    modify Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     <header className="Header text-center">
       <div className="container">
         <div className="row">
-          <h1 className="">Login with Facebook</h1>
+          <div className="col-12">
+            <h1 className="">Login with Facebook</h1>  
+          </div>
         </div>
       </div>
     </header>  
